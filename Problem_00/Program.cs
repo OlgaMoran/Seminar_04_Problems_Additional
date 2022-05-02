558a03c87b9a97d57877f65ec31393e1ce0b74e2
Olga Moran <olga@azimuth.co.za>
2022-05-02 14:52:26 +0800

    Added Problems 00 and 01

---
+++ Problem_00/Program.cs
@@ -0,0 +1,6 @@
+﻿// Найти сумму чисел от 1 до А
+
+int a = new Random().Next(2, 101);
+Console.WriteLine(a);
+int sum = (a * (1 + a)) / 2;
+Console.WriteLine("Sum: " + sum);
---
+++ Problem_01/Program.cs
@@ -0,0 +1,13 @@
+﻿// 1. Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int a = new Random().Next(1, 11);
+Console.WriteLine(a);
+int b = new Random().Next(2, 11);
+Console.WriteLine(b);
+double pow = a;
+for (int i = 1; i < b; i++)
+{
+    pow = pow * a;
+}
+Console.WriteLine(pow);
