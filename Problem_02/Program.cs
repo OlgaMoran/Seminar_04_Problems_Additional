bacf20782c5967d68c5539ebfc8a6aa310d4c0e2
Olga Moran <olga@azimuth.co.za>
2022-05-02 17:47:22 +0800

    Added Problem 02

--- Problem_02/Program.cs
+++ Problem_02/Program.cs
@@ -1,3 +1,11 @@
 ﻿// 2. Напишите программу, которая принимает на вход число 
 // и выдаёт сумму цифр в числе.
 
+int a = new Random().Next(1, 10001);
+System.Console.WriteLine();
+int sum = 0;
+for (int div = a; div > 0; div = div / 10)
+{
+    sum = sum + div % 10;
+}
+System.Console.WriteLine(sum);
